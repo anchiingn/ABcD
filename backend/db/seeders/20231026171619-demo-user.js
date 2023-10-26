f8204f1a210f9360573cd65cefdad68f5cca272e
Anchi <anchi.n248@gmail.com>
2023-10-26 12:52:05 -0500

    Add firstName and lastName

--- backend/db/seeders/20231026171619-demo-user.js
+++ backend/db/seeders/20231026171619-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'John',
+        lastName: 'Smith',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Lisa',
+        lastName: 'Ro',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Rorger',
+        lastName: 'Gold',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
